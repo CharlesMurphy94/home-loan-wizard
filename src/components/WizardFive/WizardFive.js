2f8dcd70138a254a4f84aea0c7707cb8531a33c3
Charles Murphy <cmurphy94@Charless-MacBook-Pro.local>
2017-08-08 19:04:59 -0600

    home-loan-wizard redux project - complete

--- src/components/WizardFive/WizardFive.js
+++ src/components/WizardFive/WizardFive.js
@@ -1,6 +1,7 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import { connect } from 'react-redux'
+import { updateRealEstateAgent } from './../../ducks/reducer'
 class WizardFive extends Component {
 
     render(){
@@ -10,13 +11,15 @@ class WizardFive extends Component {
 
                     <p>Are you currently working with a real estate agent?</p> <br />
                     <div className="row">
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentTrue}>Yes</button></Link>
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentFalse}>No </button></Link>
+                        <Link to="/wSix"><button onClick={(e)=>this.props.updateRealEstateAgent("True")}>Yes</button></Link>
+                        <Link to="/wSix"><button onClick={(e)=>this.props.updateRealEstateAgent("False")}>No </button></Link>
                     </div>
                 </div>
             </div>
         )
     }
 }
-
-export default WizardFive;
+function mapStateToProps(state){
+    return {realEstateAgent: state.realEstateAgent}
+}
+export default connect(mapStateToProps, {updateRealEstateAgent})(WizardFive);
